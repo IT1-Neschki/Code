3edfcb61dafbe09a1aaf3eab21ac27c2c8c7b051
It1-Raspotin <justinbosch01@googlemail.com>
2023-03-23 13:14:26 +0100

    Versuch den Email Versand einzubinden

--- Logger/RamLogger.py
+++ Logger/RamLogger.py
@@ -1,12 +1,28 @@
 import psutil
 import logging
+import datetime
+import sending_mail
+
+# Netzwerkname der Maschine
+hostname = psutil.net_if_addrs()['Ethernet'][0].address
+
+hard_limit = 40
 
 logging.basicConfig(filename='memory.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 mem = psutil.virtual_memory()
-ram_percent = mem.percent
+used_percent = mem.percent
 
-if ram_percent >= 40:
-    logging.warning(f"RAM usage is {ram_percent}%, which is over the warning threshold.")
+if used_percent >= hard_limit:
+    logging.warning(f"RAM usage is {used_percent}%, which is over the warning threshold.")
 
-logging.info(f"RAM usage: {ram_percent}%")
+logging.info(f"RAM usage: {used_percent}%")
+
+    # E-Mail-Versand bei Überschreitung des Hardlimits
+if used_percent > hard_limit:
+    message = '{} - {} - ERROR: Disk usage at {}%'.format(
+        datetime.datetime.now(), hostname, used_percent)
+    write_log(message)
+    subject = 'Hard disk space exceeded on {}'.format(hostname)
+    body = 'Disk usage is at {}%.'.format(used_percent)
+    sending_mail.send_email(subject, body)
