a938157cde50c40e8114a744c825bfb2a41e8f1c
IT1-Neschki <michel.neschki@web.de>
2023-03-23 12:51:30 +0100

    Email im eigenen Modul

---
+++ Logger/sending_mail.py
@@ -0,0 +1,17 @@
+import smtplib
+
+# E-Mail-Konfiguration
+sender_email = 'testlf8code@gmail.com'
+receiver_email = 'testlf8code@gmail.com'
+password = 'da kommt das passwort hin'
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+# Funktion zum Senden der E-Mail
+def send_email(subject, body):
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        message = 'Subject: {}\n\n{}'.format(subject, body)
+        server.sendmail(sender_email, receiver_email, message)
+
