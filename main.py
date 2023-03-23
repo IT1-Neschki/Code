35f1126d3d82d98bc7228d981b7e22f16a733606
IT1-Neschki <michel.neschki@web.de>
2023-03-23 15:37:57 +0100

    loop unterbrechung

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import subprocess
 import os
 
 logger_dir = os.path.join(os.getcwd(), "Logger")
+start_time = time.time()
+max_runtime = 300  # seconds
+loop_count = 0
 
-while True:
+while (time.time() - start_time) < max_runtime:
     subprocess.run(["python", os.path.join(logger_dir, "CPULogger.py")])
     subprocess.run(["python", os.path.join(logger_dir, "DiskLogger.py")])
     subprocess.run(["python", os.path.join(logger_dir, "RamLogger.py")])
     time.sleep(60)
+    loop_count += 1
+
+print(f"Script ran for {loop_count} loops and {time.time() - start_time} seconds.")
