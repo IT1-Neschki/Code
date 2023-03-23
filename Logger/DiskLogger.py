c6c783c3707539ee409546c066e1fcb03c905e59
IT1-Neschki <michel.neschki@web.de>
2023-03-23 12:51:57 +0100

    EMail im eigenen Moduk

--- Logger/DiskLogger.py
+++ Logger/DiskLogger.py
@@ -1,7 +1,7 @@
 import psutil
 import datetime
-import smtplib
 import os
+import sending_mail
 
 # Festlegen der Grenzwerte
 soft_limit = 2
@@ -9,19 +9,6 @@ hard_limit = 15
 
 # Netzwerkname der Maschine
 hostname = psutil.net_if_addrs()['Ethernet'][0].address
-
-# E-Mail-Konfiguration
-sender_email = 'testlf8code@gmail.com'
-receiver_email = 'testlf8code@gmail.com'
-password = '!1234567890#'
-
-# Funktion zum Senden der E-Mail
-def send_email(subject, body):
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(sender_email, password)
-        message = 'Subject: {}\n\n{}'.format(subject, body)
-        server.sendmail(sender_email, receiver_email, message)
 
 # Überprüfen und Erstellen der Logdatei
 log_file = 'log.txt'
@@ -51,4 +38,4 @@ if used_percent > hard_limit:
     write_log(message)
     subject = 'Hard disk space exceeded on {}'.format(hostname)
     body = 'Disk usage is at {}%.'.format(used_percent)
-    send_email(subject, body)
+    sending_mail.send_email(subject, body)
