1f7910ae1c0b30be07d5449532702d44679b6651
IT1-frieto <games.yusuf123@gmail.com>
2023-03-26 17:29:20 +0200

    UserLogger in Main.py hinzugefügt

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ while (time.time() - start_time) < max_runtime:
     subprocess.run(["python", os.path.join(logger_dir, "CPULogger.py")])
     subprocess.run(["python", os.path.join(logger_dir, "DiskLogger.py")])
     subprocess.run(["python", os.path.join(logger_dir, "RamLogger.py")])
+    subprocess.run(["python", os.path.join(logger_dir, "UserLogger.py")])
     time.sleep(60)
     loop_count += 1
 
